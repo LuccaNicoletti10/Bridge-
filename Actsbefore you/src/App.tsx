3623fecc98823b98a8a55e57663a07ae630b0eac
Lucca Nicoletti <luccanicoletti@Luccas-MacBook-Pro.local>
2025-11-08 11:30:26 -0300

    Remove development folders from repository

--- Actsbefore you/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { BridgeTwoSecondDemo } from './components/BridgeTwoSecondDemo';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen bg-[#0A0A0A] overflow-hidden">
-      <BridgeTwoSecondDemo />
-    </div>
-  );
-}
--- Understands How You Work/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { UnderstandsHowYouWork } from './components/UnderstandsHowYouWork';
-
-export default function App() {
-  return (
-    <div className="w-full h-screen bg-[#0A0A0A] overflow-hidden">
-      <UnderstandsHowYouWork />
-    </div>
-  );
-}
